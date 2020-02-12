2356cbe3c59f1b82a0c00e160cfb67847ca979f4
Max-Costes <mxcostes@gmail.com>
2020-02-12 17:10:09 -0500

    initial commit

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ let states = {
 
 let currentState = "green";
 
+let street = {
+  sign: 'lorem ipsum asdfa'
+
+}
+
 function enterState(newState) {
   let validTransitions = states[currentState].canChangeTo;
   if (validTransitions.includes(newState)) {
@@ -28,12 +33,27 @@ function enterState(newState) {
 
 start();
 
+async function enterFoyer(){
+  //change to state 
+  //if chain of possible actions within room
+  const foyerMessage = 'Welcome to the foyer. A small room with a staircase which you are standing at the bottom of. There is a newstand '
+  console.log(foyerMessage)
+  let input = await ask('What would you like to do?')
+}
+
+
+
 async function start() {
   const welcomeMessage = `182 Main St.
 You are standing on Main Street between Church and South Winooski.
 There is a door here. A keypad sits on the handle.
 On the door is a handwritten sign.`;
   let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+let input = await ask("What would you like to do?")
+if (input === 'read sign'){
+  console.log(street.sign)
+} if (input === "proceed to foyer"){
+  enterFoyer()
+}
   process.exit();
 }
